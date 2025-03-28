701d367637ed34922a4ad68587783c486893bd6b
Bhairavi Sawantdesai <51810658+BhairaviVSD@users.noreply.github.com>
2025-03-28 19:14:05 -0400

    Fixing

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bhairavivsd.github.io/',
+  base: '/',  
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
   }
 });
